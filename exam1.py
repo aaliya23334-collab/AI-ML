790d2ae2443d6ead52c151d26c05e4a760cee8c0
aaliya23334-collab <aaliya23334@gmail.com>
2026-04-29 23:36:26 +0530

    Add files via upload

---
+++ exam1.py
@@ -0,0 +1,43 @@
+num =[10,20,30,20,40]
+(num.count(20))
+
+num.pop(3)
+
+num.append(50)
+
+data =[5,10,15,20,25]
+(max(data))
+(min(data))
+(sum(data))
+
+items =[1,2,3]
+items.append(4)
+items.append(5)
+(items)
+items.remove(5)
+(items)
+items.reverse()
+(items)
+
+student ={"A":50,"B":60}
+student.update({"c":70})
+student.update({"A":80})
+(student)
+(student.values())
+
+nums =[1,2,2,3,3,3,4]
+frequency={items:num.count(items) for items in set(items)}
+("result:",frequency)
+
+list1 =[1,2,3]
+list2 =[3,4,5]
+list1.extend(list2)
+print(list1)
+
+
+
+
+
+
+
+
